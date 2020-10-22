77f57803a1d28e07e97c96194f91532d445e3e12
Camden Mecklem <c@camdenmecklem.com>
2020-10-22 14:44:11 -0400

    simplify python script

--- ingredients/make_ingredient_list.py
+++ ingredients/make_ingredient_list.py
@@ -2,12 +2,10 @@ with open("ingredients.txt") as f:
     ingredients = f.readlines()
 
 print("[", end='')
-for i in ingredients:
-    original = i
-    i = i[:-1]
-    if original == ingredients[-1]:
-        print(f"\"{i}\"", end='')
-    else:
-        print(f"\"{i}\", ", end='')
-print("]", end='')
 
+for i in ingredients[:-1]:
+    print(f"\"{i[:-1]}\", ", end='')
+
+print(f"\"{ingredients[-1][:-1]}\"", end='')
+
+print("]")
