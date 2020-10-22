502488b161970d3ac6ea3bad955db3d8aeca9aba
Camden Mecklem <c@camdenmecklem.com>
2020-10-22 00:15:41 -0400

    removed use of var, improved if blocks for clarity

--- cookiegen.js
+++ cookiegen.js
@@ -2,92 +2,80 @@ function cookieGen() {
     
     let ingredients = []
     let additives = ["chocolate chips", "vanilla extract", "white chocolate chips", "oregano", "lemon juice", "orange juice", "walnuts", "paprika", "red chili flakes", "thyme", "cinnamon", "rice", "beef stock", "lime juice", "honey", "olive oil", "vinegar", "garlic powder", "worcestershire sauce", "canned tuna", "oats", "feta cheese", "mozzarella cheese", "yogurt", "milk", "raisins", "sunflower seeds", "soy sauce", "sesame oil", "yeast", "sprinkles", "maple syrup", "corn syrup", "canned pumpkin", "onion powder", "bowtie pasta", "ranch dressing", "jeotgal", "garum", "honey mustard", "coconut milk", "coconut shavings", "chopped pecans", "confectioners' sugar", "ground juniper berries", "sriracha"]
-    const measurements = ["teaspoon", "tablespoon", "pinch"]
+    const measurements = ['teaspoon', 'tablespoon', 'pinch']
 
     function butterGen(){
-        let flip = Math.random();
-        if (flip < 0.5) {
-            var butterType = "salted butter"
-        } else {
-            var butterType = "unsalted butter"
-        }
-        let amount = Math.floor(Math.random() * 10)
-        var butterAmount = amount.toString() + " tablespoon";
+        const flip = Math.random();
+        const butterType = (flip < 0.5 ? 'salted butter' : 'unsalted butter')
 
-        if (amount != 1){
-            var butterAmount = butterAmount + "s";
-        }
+        const amount = Math.floor(Math.random() * 10)
+        const butterAmount = amount != 1
+            ? `${amount} tablespoons`
+            : `${amount} tablespoon`
 
         ingredients.push([butterAmount, butterType]);
     }
     
     function sugarGen(){
-        let flip = Math.random();
-        if (flip < 0.2) {
-            var sugarType = "brown sugar"
-        } else {
-            var sugarType = "regular sugar"
-        }
-        let amount = Math.floor(Math.random() * 5)
-        var sugarAmount = amount.toString() + " cup";
+        const flip = Math.random();
+        const sugarType = (flip < 0.2 ? 'brown sugar' : 'regular sugar')
 
-        if (amount != 1){
-            var sugarAmount = sugarAmount + "s";
-        }
+        let amount = Math.floor(Math.random() * 5)
+        const sugarAmount = amount != 1
+            ? `${amount} cups`
+            : `${amount} cups`
 
         ingredients.push([sugarAmount, sugarType]);
     }
 
     function flourGen(){
-        var flourType = "flour"
-        let amount = Math.floor(Math.random() * 5)
-        var flourAmount = amount.toString() + " cup";
+        const amount = Math.floor(Math.random() * 5)
+        const flourAmount = amount != 1
+            ? `${amount} cups`
+            : `${amount} cup`
 
-        if (amount != 1){
-            var flourAmount = flourAmount + "s";
-        }
-
-        ingredients.push([flourAmount, flourType]);
+        ingredients.push([flourAmount, 'flour']);
     }
 
     function bakingSodaGen(){
-        var bakingSodaType = "baking soda"
-        let amount = Math.floor(Math.random() * 2.5)
-        var bakingSodaAmount = amount.toString() + " teaspoon";
+        const amount = Math.floor(Math.random() * 2.5)
 
-        if (amount != 1){
-            var bakingSodaAmount = bakingSodaAmount + "s";
-        }
+        const bakingSodaAmount = amount != 1
+            ? `${amount} teaspoons`
+            : `${amount} teaspoon`
 
-        ingredients.push([bakingSodaAmount, bakingSodaType]);
+        ingredients.push([bakingSodaAmount, 'baking soda']);
     }
 
     function eggsGen(){
-        var eggsType ="egg"
-        var eggsAmount = Math.floor(Math.random() * 5);
+        const amount = Math.floor(Math.random() * 5);
 
-        if (eggsAmount != 1){
-            var eggsType = "eggs";
-        }
+        const eggsType = amount != 1
+            ? 'eggs'
+            : 'egg'
 
-        ingredients.push([eggsAmount, eggsType]);
+        ingredients.push([amount, eggsType]);
     }
 
     function additivesGen(){
         for (i = 0; i < 5; i++) {
             const additiveIndex = Math.floor(Math.random() * additives.length)
+
             let additiveType = additives[additiveIndex];
-            additives.splice(additiveIndex, 1);
+            additives.splice(additiveIndex, 1); //remove from the list to avoid repeats
+
             let measurement = measurements[Math.floor(Math.random() * measurements.length)];
-            if (measurement == "pinch") {
-                var amount = "1 "
+
+            let amount
+            if (measurement === 'pinch') {
+                amount = '1 '
             } else {
-                var amount = (Math.floor(Math.random() * 8)).toString() + " ";
+                amount = (Math.floor(Math.random() * 8)).toString() + ' ';
                     while (amount == 0) {
-                        var amount = (Math.floor(Math.random() * 8)).toString() + " ";
+                        amount = (Math.floor(Math.random() * 8)).toString() + ' ';
                     }
                 if (amount != 1) {
-                    measurement += "s";
+                    measurement += 's';
                 }
             }
 
@@ -103,7 +91,7 @@ function cookieGen() {
     eggsGen();
     additivesGen();
 
-    const table = document.getElementById("recipe")
+    const table = document.getElementById('recipe')
     const tableRef = table.getElementsByTagName('tbody')[0];
     for (i = 0; i < (ingredients.length); i++) {
         const row = tableRef.insertRow(-1);
@@ -115,8 +103,7 @@ function cookieGen() {
 }
 
 
-// document.getElementById("init").addEventListener("click", cookieGen);
 cookieGen();
 
-document.getElementById("init").addEventListener("click", () => location.reload());
-document.getElementById("init").style.display = "inline-block";
+document.getElementById('init').addEventListener('click', () => location.reload());
+document.getElementById('init').style.display = 'inline-block';
