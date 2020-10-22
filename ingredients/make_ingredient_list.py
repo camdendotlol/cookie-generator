14b00349ec732839689e9c8cd9d2559239ac0c7d
Camden Mecklem <c@camdenmecklem.com>
2020-10-22 14:47:10 -0400

    commented code for clarity

--- ingredients/make_ingredient_list.py
+++ ingredients/make_ingredient_list.py
@@ -1,7 +1,10 @@
 with open("ingredients.txt") as f:
-    ingredients = f.readlines()
+    ingredients = f.readlines() # TODO: strip newlines as list is brought in
 
 print("[", end='')
+
+# there is a newline character at the end of each ingredient
+# use ingredient[:-1] to avoid newline
 
 for i in ingredients[:-1]:
     print(f"\"{i[:-1]}\", ", end='')
